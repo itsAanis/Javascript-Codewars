1d594db382132155486eb3d769b1f33548978d7c
itsAanis <aanishassan@hotmail.com>
2022-05-09 21:16:57 +0100

    CC 09-05-22 updated with codewar solution

--- CC 09-05-22 theOneBefore.js
+++ CC 09-05-22 theOneBefore.js
@@ -1,1 +1,22 @@
-let keep = "keep it up"
+/*   Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+
+*/
+
+function opposite(number) {
+    return Math.abs(num)*-1
+  } 
+  // Math.abs method returns the absolute value of a number
+
+  // or can write it as below EASYYYYY
+
+  function opposite(number) {
+ 
+    return number * -1
+  }
+  // any any number multiplied by -1 would return that number as a minus 
