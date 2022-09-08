37732cd63e2df4fa9df4007592dd091da6f136ec
itsAanis <aanishassan@hotmail.com>
2022-09-08 09:26:59 +0100

    codewars completed

---
+++ basic-obj.js
@@ -0,0 +1,23 @@
+/*Task
+Give you a function animal, accept 1 parameter:obj like this:
+
+{name:"dog",legs:4,color:"white"}
+and return a string like this:
+
+"This white dog has 4 legs."      */
+
+
+
+
+function animal(obj){
+    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
+    }
+
+
+ const word = {
+    name : 'dog',
+    legs : 4 ,
+    color : 'white'
+ }   
+
+ console.log(animal(word))
